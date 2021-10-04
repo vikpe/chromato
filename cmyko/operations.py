33fd5dce7e0625ed75a3b92df598e56d72ba1dc3
Viktor Persson <viktor.persson@arcsin.se>
2021-10-04 13:02:06 +0200

    Refactor operations.

--- cmyko/operations.py
+++ cmyko/operations.py
@@ -13,14 +13,15 @@ def blend(color1: Color, color2: Color, factor: float = 0.5) -> Color:
 
 
 def shade(color: Color, factor: float) -> Color:
-    return blend(color, Color(RGB(RGB_MIN, RGB_MIN, RGB_MIN)), factor=factor)
+    black = Color(RGB(RGB_MIN, RGB_MIN, RGB_MIN))
+    return blend(color, black, factor=factor)
 
 
 def tone(color: Color, factor: float) -> Color:
-    return blend(
-        color, Color(RGB(RGB_MAX / 2, RGB_MAX / 2, RGB_MAX / 2)), factor=factor
-    )
+    gray = Color(RGB(RGB_MAX / 2, RGB_MAX / 2, RGB_MAX / 2))
+    return blend(color, gray, factor=factor)
 
 
 def tint(color: Color, factor: float) -> Color:
-    return blend(color, Color(RGB(RGB_MAX, RGB_MAX, RGB_MAX)), factor=factor)
+    white = Color(RGB(RGB_MAX, RGB_MAX, RGB_MAX))
+    return blend(color, white, factor=factor)
