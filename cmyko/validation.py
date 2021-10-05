ea8b58707e7455c4ff63661ff32b9a59f3daafe0
Viktor Persson <viktor.persson@arcsin.se>
2021-10-05 16:16:56 +0200

    Add is_valid_rgb_value().

--- cmyko/validation.py
+++ cmyko/validation.py
@@ -1,7 +1,23 @@
 import string
 
+from . import constants
+
 
 def is_valid_hex(value) -> bool:
     _hex = str(value).lstrip("#")
 
     return len(_hex) in (3, 6) and all(c in string.hexdigits for c in _hex)
+
+
+def is_valid_rgb_value(value) -> bool:
+    if value is None or value is False:
+        return False
+
+    try:
+        return (
+            constants.RGB_MIN
+            <= round(float(value), constants.RGB_PRECISION)
+            <= constants.RGB_MAX
+        )
+    except:
+        return False
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -17,3 +17,20 @@ def test_is_valid_hex():
     assert validation.is_valid_hex("ab") is False
     assert validation.is_valid_hex("abcd") is False
     assert validation.is_valid_hex("abcde") is False
+
+
+def test_is_valid_rgb_value():
+    # valid
+    assert validation.is_valid_rgb_value(0) is True
+    assert validation.is_valid_rgb_value(127.5) is True
+    assert validation.is_valid_rgb_value(255) is True
+    assert validation.is_valid_rgb_value("255") is True
+    assert validation.is_valid_rgb_value("255.0") is True
+
+    # invalid
+    assert validation.is_valid_rgb_value(-0.1) is False
+    assert validation.is_valid_rgb_value(256) is False
+    assert validation.is_valid_rgb_value("a") is False
+    assert validation.is_valid_rgb_value(None) is False
+    assert validation.is_valid_rgb_value([]) is False
+    assert validation.is_valid_rgb_value(()) is False
