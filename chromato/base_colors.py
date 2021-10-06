5c27ec844926d38f035d932cafe5dcbc82fc1c49
Viktor Persson <viktor.persson@arcsin.se>
2021-10-06 17:12:47 +0200

    Add base_colors module.

---
+++ chromato/base_colors.py
@@ -0,0 +1,11 @@
+from . import classes
+
+white = classes.Color(255, 255, 255)
+gray = classes.Color(255 / 2, 255 / 2, 255 / 2)
+black = classes.Color(0, 0, 0)
+red = classes.Color(255, 0, 0)
+green = classes.Color(0, 255, 0)
+blue = classes.Color(0, 0, 255)
+cyan = classes.Color(0, 255, 255)
+magenta = classes.Color(255, 0, 255)
+yellow = classes.Color(255, 255, 0)
