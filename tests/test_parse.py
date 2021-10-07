fa79b9abbfd7c505470533d96157f24e7ba99c8c
Viktor Persson <viktor.persson@arcsin.se>
2021-10-07 11:46:54 +0200

    Update tests for parse_hex().

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -6,6 +6,7 @@ from chromato import parse, spaces
 def test_parse_hex():
     assert parse.parse_hex("ff00ff") == "ff00ff"
     assert parse.parse_hex("#ff00ff") == "ff00ff"
+    assert parse.parse_hex("f") == "ffffff"
     assert parse.parse_hex("f0f") == "ff00ff"
     assert parse.parse_hex("#f0f") == "ff00ff"
 
