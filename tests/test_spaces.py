03384d3a26a023b08068e7c6cc28d062ea006175
Viktor Persson <viktor.persson@arcsin.se>
2021-10-12 16:11:56 +0200

    Refactor tests for spaces.

--- tests/test_spaces.py
+++ tests/test_spaces.py
@@ -1,26 +1,26 @@
-from chromato import spaces
+from chromato.spaces import Color, CMYK, HEX, HLS, HSV, RGB
 
 
 def test_hex():
-    assert "ff0000" == spaces.HEX("f00") == spaces.HEX(spaces.HEX("f00"))
+    assert "ff0000" == HEX("f00") == HEX(HEX("f00"))
 
 
 def test_color_class_methods():
-    c_hex = spaces.Color.from_hex("f00")
-    c_cmyk = spaces.Color.from_cmyk(0, 100, 100, 0)
-    c_hls = spaces.Color.from_hls(0, 0.5, 1)
-    c_hsv = spaces.Color.from_hsv(0, 1, 1)
-    c_rgb = spaces.Color.from_rgb(255, 0, 0)
+    c_hex = Color.from_hex("f00")
+    c_cmyk = Color.from_cmyk(0, 100, 100, 0)
+    c_hls = Color.from_hls(0, 0.5, 1)
+    c_hsv = Color.from_hsv(0, 1, 1)
+    c_rgb = Color.from_rgb(255, 0, 0)
     assert c_hex == c_cmyk == c_hls == c_hsv == c_rgb
 
 
 def test_color_properties():
-    color = spaces.Color(255, 0, 0)
-    assert color.rgb == spaces.RGB(255, 0, 0)
-    assert color.hex == spaces.HEX("ff0000")
-    assert color.cmyk == spaces.CMYK(0, 100, 100, 0)
-    assert color.hls == spaces.HLS(0, 0.5, 1)
-    assert color.hsv == spaces.HSV(0, 1, 1)
-    assert color == spaces.Color("ff0000")
+    color = Color(255, 0, 0)
+    assert color.rgb == RGB(255, 0, 0)
+    assert color.hex == HEX("ff0000")
+    assert color.cmyk == CMYK(0, 100, 100, 0)
+    assert color.hls == HLS(0, 0.5, 1)
+    assert color.hsv == HSV(0, 1, 1)
+    assert color == Color("ff0000")
     assert color != "255 0 0"
     assert repr(color) == "Color(r=255, g=0, b=0)"
